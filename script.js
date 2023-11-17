f5540e0b9f1331820b63c5c173c12c226ed176d7
stephen-scheiman <sbscheiman@gmail.com>
2023-11-17 15:59:16 -0800

    Scoreboard working-ish

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var wordChoiceList = ["orange", "door", "front", "zebra","fungus"];
+var wordChoiceList = ["orange", "door", "front", "zebra", "fungus"];
 var index = Math.floor(Math.random() * wordChoiceList.length);
 var wordChoice = wordChoiceList[index];
 var wordChoiceArray = wordChoice.split("");
 var hiddenArray = [];
+var wins = 0;
+var losses = 0;
 
 for (i = 0; i <= wordChoice.length - 1; i++) {
   hiddenArray[i] = `_ `;
 }
-secretWord.textContent = hiddenArray.join('');
+secretWord.textContent = hiddenArray.join("");
+
 function letterChoice(event) {
   //assigns selected letter to a variable
   var keyPress = event.key;
   var keyCode = event.code;
   // Updates content on page
- //document.querySelector("#key").textContent = keyPress;
+  //document.querySelector("#key").textContent = keyPress;
 
   for (i = 0; i <= wordChoice.length - 1; i++) {
     if (keyPress === wordChoiceArray[i]) {
@@ -22,12 +25,17 @@ function letterChoice(event) {
     }
   }
 
-  console.log(hiddenArray.join(""));
   secretWord.textContent = hiddenArray.join("");
 
-  if(wordChoice === hiddenArray.join('')){
+  if (wordChoice === hiddenArray.join("")) {
     console.log("Holy Shit!!!!");
+    wins++;
+    keepScore(wins);
   }
+}
+
+function keepScore(wins) {
+  document.querySelector("#wins").textContent = "Wins: " + wins;
 }
 
 // Adds listener for keydown event

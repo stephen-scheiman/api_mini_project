e30e4f826c1f5d89a898fa8906a2709139368306
stephen-scheiman <sbscheiman@gmail.com>
2023-11-17 15:04:51 -0800

    Initial file upload and basic rough functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+var wordChoiceList = ["orange", "door", "front", "zebra"];
+var index = Math.floor(Math.random() * wordChoiceList.length);
+var wordChoice = wordChoiceList[index];
+var wordChoiceArray = wordChoice.split("");
+var hiddenArray = [];
+
+for (i = 0; i <= wordChoice.length - 1; i++) {
+  hiddenArray[i] = `_`;
+}
+
+function letterChoice(event) {
+  //assigns selected letter to a variable
+  var keyPress = event.key;
+  var keyCode = event.code;
+  // Updates content on page
+  document.querySelector("#key").textContent = keyPress;
+
+  for (i = 0; i <= wordChoice.length - 1; i++) {
+    if (keyPress === wordChoiceArray[i]) {
+      secretWord.textContent = keyPress;
+      hiddenArray[i] = keyPress;
+    }
+  }
+
+  console.log(hiddenArray.join(""));
+  secretWord.textContent = hiddenArray.join("");
+
+  if(wordChoice === hiddenArray.join('')){
+    console.log("Holy Shit!!!!");
+  }
+}
+
+// Adds listener for keydown event
+document.addEventListener("keydown", letterChoice);

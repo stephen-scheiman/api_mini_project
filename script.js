c4d2a9fcecf288440c7252ebdb901e968e056563
stephen-scheiman <sbscheiman@gmail.com>
2023-11-17 15:22:51 -0800

    slight tweaks to MVP, final commit to main

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var wordChoiceList = ["orange", "door", "front", "zebra"];
+var wordChoiceList = ["orange", "door", "front", "zebra","fungus"];
 var index = Math.floor(Math.random() * wordChoiceList.length);
 var wordChoice = wordChoiceList[index];
 var wordChoiceArray = wordChoice.split("");
 var hiddenArray = [];
 
 for (i = 0; i <= wordChoice.length - 1; i++) {
-  hiddenArray[i] = `_`;
+  hiddenArray[i] = `_ `;
 }
-
+secretWord.textContent = hiddenArray.join('');
 function letterChoice(event) {
   //assigns selected letter to a variable
   var keyPress = event.key;
   var keyCode = event.code;
   // Updates content on page
-  document.querySelector("#key").textContent = keyPress;
+ //document.querySelector("#key").textContent = keyPress;
 
   for (i = 0; i <= wordChoice.length - 1; i++) {
     if (keyPress === wordChoiceArray[i]) {
